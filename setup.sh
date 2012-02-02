f3286e4e0257dfb3b837bbb9f45533fb407fe395
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-02-02 19:10:13 +0000

    more junk

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ LIBS=`pwd`/lib:`geant4-config --prefix`/lib:`clhep-config --prefix`/lib
 
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${LIBS}
 export PYTHONPATH=$PYTHONPATH:${LIBS}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIBS}
