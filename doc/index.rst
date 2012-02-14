0244191bf580baeac4f159df64484eb6d982e995
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-02-14 10:06:32 +0000

    add docs

---
+++ doc/index.rst
@@ -0,0 +1,22 @@
+.. gnomon documentation master file, created by
+   sphinx-quickstart on Tue Feb 14 10:06:14 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to gnomon's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- reconfigure.sh
+++ reconfigure.sh
@@ -3,4 +3,4 @@ export PYTHON_PREFIX=${HOME}/.cache/0install.net/implementations/sha1new=4acc3af
 
 export XD=/Users/tunnell/.cache/0install.net/implementations/sha1new=d89a1862c49e0613589ee865103afa0d01fa8494
 
-./configure macosx --with-g4-libdir=${GEANT_PREFIX}/lib --with-clhep-incdir=`clhep-config --prefix`/include --with-clhep-libdir=`clhep-config --prefix`/lib/ --with-boost-incdir=/opt/local/include --with-boost-libdir=/opt/local/lib --with-g4-incdir=${GEANT_PREFIX}/include/Geant4 --with-xercesc-incdir=${XD}/include --with-xercesc-libdir=${XD}/lib --with-python-incdir=${PYTHON_PREFIX}/lib --with-python-libdir=${PYTHON_PREFIX}/include --with-python3
+./configure macosx --with-g4-libdir=${GEANT_PREFIX}/lib --with-clhep-incdir=`clhep-config --prefix`/include --with-clhep-libdir=`clhep-config --prefix`/lib/ --with-boost-incdir=/opt/local/include --with-boost-libdir=/opt/local/lib --with-g4-incdir=${GEANT_PREFIX}/include/Geant4 --with-xercesc-incdir=${XD}/include --with-xercesc-libdir=${XD}/lib --with-python-incdir=${PYTHON_PREFIX}/include/python3.2m --with-python-libdir=${PYTHON_PREFIX}/lib/python3.2 --with-python3
