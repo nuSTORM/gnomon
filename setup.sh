9fe84e225d3c89ffd6510c4f857f7833f2d0f6cb
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-02-16 12:48:45 +0000

    trying to switch back to py2.7 after py3 disasters

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
+export GNOMON_ENV=/Users/tunnell/env/gnomon
+
+source ${GNOMON_ENV}/bin/activate
+
 #source /Users/tunnell/work/genie-trunk2/root/bin/thisroot.sh
 
-XLIB=~/.cache/0install.net/implementations/sha1new=7b74a95224b4470762beb7f4de4a3a14a021e47d/lib
+export XLIB=/Users/tunnell/.cache/0install.net/implementations/sha1new=3e6859300df7f1a7458a61613d21ee2d0cbe3f0a/lib/
 LIBS=`pwd`/lib:`geant4-config --prefix`/lib:`clhep-config --prefix`/lib:${XLIB}
 
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${LIBS}
