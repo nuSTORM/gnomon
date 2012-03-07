9945e9fc4e62e683e65dc1fbb64672822316ead4
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-03-07 21:52:16 +0000

    Tweaking batch scripts

--- bin/malco_name.py
+++ bin/malco_name.py
@@ -2,20 +2,22 @@ import couchdb
 import urlparse
 import os
 
-remote_url = 'http://tasd.fnal.gov:5984/'
+remote_url = os.getenv('COUCHDB_URL')
 remote_couch = couchdb.Server(remote_url)
 
-local_url = 'http://gnomon:harry@gnomon.iriscouch.com/'
+local_url = 'http://gnomon:balls@127.0.0.1:5984'
 local_couch =couchdb.Server(local_url)
 
 for momentum in [100, 139, 195, 271, 379, 528, 737, 1028, 1434, 2000]:
     for pid in [-13, 13]:
         db_name = 'malcolm_%d_%d' % (momentum, pid)
         print db_name
+        continue
 
         print 'remote', remote_url
         print 'local', local_url
         
+        db_name = 'malcolm_%d_%d' % (momentum, pid)
         local_link = urlparse.urljoin(local_url, db_name)
         
         dest_link =  urlparse.urljoin(remote_url, db_name)
