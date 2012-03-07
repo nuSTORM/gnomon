fda910b2addb3e5356187fddc3961a1997563df7
tunnell@pplxint6.physics.ox.ac.uk <tunnell@pplxint6.physics.ox.ac.uk>
2012-03-07 15:06:32 +0000

    Fixed case of missing Y hits in bulk mode

--- bin/malcolm.py
+++ bin/malcolm.py
@@ -5,7 +5,7 @@ import couchdb
 
 couch = couchdb.Server(os.getenv('COUCHDB_URL'))
 
-number_of_events = 100
+number_of_events = 1000
 
 for momentum in [100, 139, 195, 271, 379, 528, 737, 1028, 1434, 2000]:
     for pid in [-13, 13]:
@@ -14,7 +14,7 @@ for momentum in [100, 139, 195, 271, 379, 528, 737, 1028, 1434, 2000]:
             filename = tempfile.mkstemp()[1]
             file = open(filename, 'w')
         
-            db_name = 'malcolm_tiny_%d_%d' % (momentum, pid)
+            db_name = 'malcolm_%d_%d' % (momentum, pid)
             try:
                 couch.delete(db_name)
             except:
@@ -23,7 +23,7 @@ for momentum in [100, 139, 195, 271, 379, 528, 737, 1028, 1434, 2000]:
             script = """
 source /home/tunnell/env/gnomon/bin/activate
 cd $VIRTUAL_ENV/src/gnomon
-time python simulate.py --name %(db_name)s --vertex 0 2000 0 -p --momentum 0 0 %(momentum)d --events %(number_of_events)d --logfileless --pid %(pid)d --log_level INFO --run %(run)d
+time python simulate.py --name %(db_name)s --uniform -p --momentum 0 0 %(momentum)d --events %(number_of_events)d --logfileless --pid %(pid)d --log_level INFO --run %(run)d
 time python digitize.py --name %(db_name)s -a
 """ % {'momentum': momentum, 'db_name' : db_name, 'number_of_events' : number_of_events, 'pid' : pid, 'run' : run}
 
