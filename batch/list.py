40c677e6bd8d2242f787349df5b40c25aba0c943
tunnell@pplxint6.physics.ox.ac.uk <tunnell@pplxint6.physics.ox.ac.uk>
2012-03-19 19:04:45 +0000

    fixing up list of dbs

--- batch/list.py
+++ batch/list.py
@@ -9,6 +9,10 @@ couch = couchdb.Server(server)
 
 for momentum in batch_queue_config.momenta:
     for pid in batch_queue_config.pids:
-        db_name = 'malcolm_%d_%d' % (momentum, pid)
+        for polarity in ['-', '+']:
+            if polarity == '-':
+                db_name = 'malcolm_minus_%d_%d' % (momentum, pid)
+            else:
+                db_name = 'malcolm_plus_%d_%d' % (momentum, pid)
 
         print db_name
