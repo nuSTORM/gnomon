7ae317dfca57ec1a6e54f9ca27499b30d13d098d
tunnell@pplxint7.physics.ox.ac.uk <tunnell@pplxint7.physics.ox.ac.uk>
2012-04-06 21:03:05 +0100

    generate genie

---
+++ data/create_simple_flux.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+import time
+import math
+import sys
+
+def frange(startValue, endValue, steps):
+    incrementValue = (float(endValue) - startValue) / steps
+    for i in range(steps):
+        yield float(startValue) + i*incrementValue
+
+polarization = 0.0
+pi = 3.1415
+parent_energy = 2.0 # GeV 
+
+E_min = 0.10  # GeV
+E_max = parent_energy # GeV
+E_steps = 500
+
+mass_muon = 0.1056 # GeV
+
+stored_muons = 1e17
+
+theta = 0
+
+distance = 1 # km, GLoBES normalization
+
+flux_file_e = open('flux_file_e.dat', 'w')
+flux_file_mu = open('flux_file_mu.dat', 'w')
+
+for E in frange (E_min, E_max, E_steps):
+    cos_theta = 1
+    y = float(E) / parent_energy
+    beta = math.sqrt(1 - mass_muon**2 / parent_energy**2)
+
+    N = stored_muons
+    N /= pi * distance**2 * mass_muon**6
+    N *= parent_energy**4 * y**2 * (1 - beta * math.cos(theta))
+
+    flux_mu = 4 * N * (3 * mass_muon**2 - 4 * parent_energy**2 * y * (1 - beta * math.cos(theta)))
+    flux_e = 24 * N * (1 * mass_muon**2 - 2 * parent_energy**2 * y * (1 - beta * math.cos(theta)))
+    
+    
+    flux_e  *= (2*pi) / parent_energy  # solid angle -> cos(theta), y -> E
+    flux_mu *= (2*pi) / parent_energy  # solid angle -> cos(theta), y -> E  
+
+    flux_file_e.write('%0.9g %0.9g\n' % (E, flux_e))
+    flux_file_mu.write('%0.9g %0.9g\n' % (E, flux_mu))
+
+
+flux_file_e.close()
+flux_file_mu.close()
+
