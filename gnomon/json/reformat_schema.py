fefc8d6d7a4166e00efbd5ee0d52827a5db05864
tunnell@pplxint6.physics.ox.ac.uk <tunnell@pplxint6.physics.ox.ac.uk>
2012-07-26 16:42:49 +0100

    JSON reformatter
    --HG--
    rename : gnomon/json/reformat_schema.sh => gnomon/json/reformat_schema.py

---
+++ gnomon/json/reformat_schema.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+from glob import glob
+import json
+
+for file in glob('*.json'):
+  f = open(file, 'r')
+  text = f.read()
+  f.close()
+
+  dict = json.loads(text)
+  new_text = json.dumps(dict, sort_keys=True, indent=4)
+
+  f = open('%s.bak' % file, 'w')
+  f.write(text)
+  f.close()
+
+  f = open(file, 'w')
+  f.write(new_text)
+  f.close()
+
--- gnomon/json/reformat_schema.sh
+++
@@ -1,3 +0,0 @@
-FILENAME=ConfigurationSchema.json
-cat $FILENAME | python -mjson.tool > ${FILENAME}2
-mv ${FILENAME}2 $FILENAME
