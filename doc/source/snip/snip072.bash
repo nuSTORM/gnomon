3bbf6ac36a731a659e89e73a866605d2978b536b
tunnell@pplxint6.physics.ox.ac.uk <tunnell@pplxint6.physics.ox.ac.uk>
2012-08-20 08:21:02 +0100

    Documentation now makes build script

--- doc/source/snip/snip072.bash
+++ doc/source/snip/snip072.bash
@@ -1,3 +1,4 @@
+cd $VIRTUAL_ENV
 tar xvf root_v5.32.00.source.tar.gz
 mv root $VIRTUAL_ENV/src/root-5.32.00
 cd $VIRTUAL_ENV/src/root-5.32.00
--- doc/source/snip/snip12.bash
+++ doc/source/snip/snip12.bash
@@ -5,18 +5,17 @@ cd genie
 echo export GENIE=\$VIRTUAL_ENV/src/genie >> $VIRTUAL_ENV/bin/activate
 echo export PYTHIA6=\$VIRTUAL_ENV/src/pythia6/v6_424/lib >> $VIRTUAL_ENV/bin/activate
 echo export LHAPDF=\${GENIE}/v5_8_8/stage >> $VIRTUAL_ENV/bin/activate
-echo export LOG4CPP=\${GENIE}/v1_0/stage >> $VIRTUAL_ENV/bin/activate
 echo export PATH=\$PATH:\${LHAPDF}/bin >> $VIRTUAL_ENV/bin/activate
 echo export PYTHONPATH=\$PYTHONPATH:\${LHAPDF}/lib/python2.6/site-packages/ >> $VIRTUAL_ENV/bin/activate
 echo export LHAPATH=\`lhapdf-config --pdfsets-path\` >> $VIRTUAL_ENV/bin/activate
 echo export LLP=LD_LIBRARY_PATH >> $VIRTUAL_ENV/bin/activate
-echo eval \${LLP}=\${!LLP}:\${LHAPDF}/lib:\${GENIE}/lib:\${PYTHIA6}/lib:/opt/local/lib:\$LOG4CPP/lib >> $VIRTUAL_ENV/bin/activate
+echo eval \${LLP}=\${!LLP}:\${LHAPDF}/lib:\${GENIE}/lib:\${PYTHIA6}/lib:/opt/local/lib >> $VIRTUAL_ENV/bin/activate
 
 source $VIRTUAL_ENV/bin/activate
 
 ./src/scripts/build/ext/build_lhapdf.sh 5.8.8 --refetch
 
-./src/scripts/build/ext/build_log4cpp.sh 
+source $VIRTUAL_ENV/bin/activate
 
 ./configure --with-lhapdf-lib=$LHAPDF/lib --with-lhapdf-inc=$LHAPDF/include
 
