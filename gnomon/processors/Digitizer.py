e909126b08c2f8ac304fbc11a349a5902740a9e7
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-09-24 21:55:36 +0100

    Document Digitizer.py

--- gnomon/processors/Digitizer.py
+++ gnomon/processors/Digitizer.py
@@ -10,36 +10,42 @@ class VlenfSimpleDigitizer(Base.Processor):
     generic energy scale."""
 
     def __init__(self):
-        """Initialize with defaults
+        """Initialize with defaults digitization files
         """
         Base.Processor.__init__(self)
 
         self.energy_scale = 80.0  # pe / MeV
         self.log.debug('Energy scale: %f', self.energy_scale)
 
-        self.threshold = None
+        self.threshold = None  # [ADC counts], to be set on next line
         self.set_threshold()
 
     def process(self, docs):
-        new_docs = []
-        hits_dict = {}
+        new_docs = []  # Modified documents
+        hits_dict = {}  # Used to combine hits in same bar
 
         for doc in docs:
+            #  For every document, see if it's an MC hit
             if doc['type'] != 'mchit':
                 new_docs.append(doc)
                 continue
 
+            # If MC hit, then extract where the hit was
             layer = doc['layer']
             view = doc['view']
             position_bar = doc['position_bar']
 
+            # Form unique location key
             key = (doc['run'], doc['event'], layer, doc['bar'], view)
 
+            #  If first hit here, create array to store these hits
             if key not in hits_dict.keys():
                 hits_dict[key] = []
 
+            #  Determine what is 'seen'
             counts_adc = doc['dedx'] * self.energy_scale
 
+            #  Create digit
             digit = {}
             digit['type'] = 'digit'
             digit['run'] = doc['run']
@@ -50,16 +56,20 @@ class VlenfSimpleDigitizer(Base.Processor):
             digit['counts_adc'] = counts_adc
             digit['position'] = position_bar  # this should be derived
 
+            #  Append digit to list of hits for this bar
             hits_dict[key].append(digit)
 
+        # Combine hits from same bar
         partial_digit = None
         for key, value in hits_dict.iteritems():
+            # Sum hits in bar
             total_counts_adc = 0
             for partial_digit in value:
                 total_counts_adc += partial_digit['counts_adc']
 
             total_counts_adc = int(total_counts_adc)
 
+            #  Only save if passes threshold
             if total_counts_adc > self.get_threshold():
                 if partial_digit is not None:
                     partial_digit['counts_adc'] = total_counts_adc
@@ -70,7 +80,7 @@ class VlenfSimpleDigitizer(Base.Processor):
     def set_threshold(self, threshold=2):
         """Threshold for registering a hit
 
-        Units are ADC counts"""
+        Units are adc counts"""
         self.threshold = threshold
 
     def get_threshold(self):
