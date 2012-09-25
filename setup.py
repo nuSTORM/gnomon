b6db1abe52c528b1a21e0e875f3f5c91761d1013
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-09-25 04:46:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gnomon',
+    version='v0.1',
+    packages=['gnomon', 'gnomon.processors'],
+    url='https://github.com/nuSTORM/gnomon',
+    license='LICENSE.txt',
+    author='tunnell',
+    author_email='c.tunnell1@physics.ox.ac.uk',
+    description=''
+)
