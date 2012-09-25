136d6a43242358c44cbdf8dfccd7263b3fe70b53
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-09-25 05:44:54 +0100

    Requirements read out of file that readthedocs.org can see

--- setup.py
+++ setup.py
@@ -2,16 +2,10 @@ from distutils.core import setup
 
 def parse_requirements(file_name):
     requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
+    for line in open(file_name, 'r').readlines():
+        requirements.append(line.rstrip('\n'))
 
+    print requirements
     return requirements
 
 setup(
@@ -22,6 +16,7 @@ setup(
     license='LICENSE.txt',
     author='tunnell',
     author_email='c.tunnell1@physics.ox.ac.uk',
-    description='',
-    requires = parse_requirements('requirements.txt')
+    long_description=open('README.rst').read(),
+    description='desc',
+    install_requires = parse_requirements('requirements.txt')
 )
