9117dd43c4a9795a477b15cc828be8d2d3453110
Christopher Tunnell <c.tunnell1@physics.ox.ac.uk>
2012-09-25 05:30:56 +0100

    Add requirements file

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from distutils.core import setup
 
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
 setup(
     name='gnomon',
     version='v0.1',
@@ -9,8 +23,5 @@ setup(
     author='tunnell',
     author_email='c.tunnell1@physics.ox.ac.uk',
     description='',
-    requires = [
-               'python-graph-core',
-               'validictory',
-               ],
+    requires = parse_requirements('requirements.txt')
 )
